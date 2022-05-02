9f055c56f2205e18c7f7eba0eb45319b95540a82
ljwoon <ljwoon@nsmarts.co.kr>
2022-05-02 09:24:12 +0900

    updage

--- src/app/components/web-rtc/web-rtc.component.ts
+++ src/app/components/web-rtc/web-rtc.component.ts
@@ -719,8 +719,8 @@ export class WebRTCComponent implements OnInit {
         if (navigator.mediaDevices.getDisplayMedia) {
 			console.log('navigator.mediaDevices.getDisplayMedia')
             console.log(this.audioDeviceExist)
-			let screenStream = await navigator.mediaDevices.getDisplayMedia({
-				// audio: true,
+			let mediaStream = await navigator.mediaDevices.getDisplayMedia({
+				audio: false,
                 video: true
 			})
             // 오디오 장치가 있을 경우
@@ -730,7 +730,7 @@ export class WebRTCComponent implements OnInit {
                     audio:true
                 })
                 // 오디오랑 비디오 스트림 결합
-                screenStream = await new MediaStream( [audio.getTracks()[0], screenStream.getTracks()[0] ])
+                const screenStream = await new MediaStream( [audio.getTracks()[0], mediaStream.getTracks()[0] ])
                 if(screenStream){
                     callback(screenStream)
                 } else {
@@ -738,9 +738,9 @@ export class WebRTCComponent implements OnInit {
                 }
             // 오디오 장치가 없을 경우
             } else {
-                if(screenStream){
+                if(mediaStream){
                     // 미디어 스트림만 바로 전송
-                    callback(screenStream)
+                    callback(mediaStream)
                 } else {
                     callback('cancel')
                 }
